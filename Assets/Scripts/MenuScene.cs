3fbc20df1c31b74ffffe19b2844575ae0d3d7dcb
Liam Beckingham <Lj.beckingham99@gmail.com>
2021-12-16 22:29:30 +0000

    First commit, primative mobile app and .gitignore

---
+++ Assets/Scripts/MenuScene.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MenuScene : MonoBehaviour
+{
+    private CanvasGroup fadeGroup;
+    private float fadeInSpeed = 0.33f; // multiplied by time.time if (10 seconds 0.1)
+    
+
+    private void Start()
+    {
+        // Get CanvasGroup in scene
+        fadeGroup = FindObjectOfType<CanvasGroup>();
+
+        // Start on white screen
+        fadeGroup.alpha =1;
+    }
+
+    private void Update()
+    {
+        // Fade in
+        fadeGroup.alpha = 1 - Time.timeSinceLevelLoad * fadeInSpeed;
+
+        //
+
+    }
+
+    // Buttons
+    public void OnARClick()
+    {
+        Debug.Log("AR Button has been clicked!");
+        SceneManager.LoadScene("AR");
+        
+    }
+
+    public void OnInfoClick()
+    {
+        Debug.Log("Info Button has been clicked!");
+    }
+
+    public void OnMappingClick()
+    {
+        Debug.Log("Mapping Button has been clicked!");
+    }
+
+}
---
+++ Assets/Scripts/PreLoader.cs
@@ -0,0 +1,50 @@
+﻿/*
+    Refrenece - https://www.youtube.com/watch?v=sZhhfOH0Q3Y
+*/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PreLoader : MonoBehaviour
+{
+    private CanvasGroup fadeGroup;
+    private float loadTime;
+    private float minimumLogoTime = 3.0f;
+
+    private void Start()
+    {
+        // Get CanvasGroup in scene
+        fadeGroup = FindObjectOfType<CanvasGroup>();
+
+        // Start on white screen
+        fadeGroup.alpha = 1;
+
+        // Preload app
+        // LOAD ALL DEPENDENCIES HERE
+
+        if (Time.time < minimumLogoTime)
+            loadTime = minimumLogoTime;
+        else
+            loadTime =Time.time;
+    }
+
+    private void Update()
+    {
+        // Fade in
+        if (Time.time < minimumLogoTime)
+        {
+            fadeGroup.alpha = 1 - Time.time;
+        }
+
+        // Fade out
+        if (Time.time > minimumLogoTime && loadTime != 0 ){
+            fadeGroup.alpha = Time.time - minimumLogoTime;
+            if(fadeGroup.alpha >= 1)
+            {
+                SceneManager.LoadScene("Menu");
+            }
+        }
+    }
+}
