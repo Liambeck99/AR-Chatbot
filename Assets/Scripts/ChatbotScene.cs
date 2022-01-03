b31369171d271e6d2fd2f8624ddbf7791cd317e6
Lewis Stuart <lewis.stuart11@yahoo.co.uk>
2022-01-03 17:51:10 +0000

    Added new conversation renderer script which handles rendering speechbubbles on the screen and commented code

--- Assets/Scripts/ChatbotScene.cs
+++ Assets/Scripts/ChatbotScene.cs
@@ -8,66 +8,30 @@ public class ChatbotScene : BaseUIScene
 {
     private ConversationHandler currentConversation;
 
-    private float currentConversationHeadPosition;
-
-    private GameObject templateUserSpeechBubble;
-
-    private GameObject templateChatbotSpeechBubble;
-
-    private float distanceBetweenSpeechBubbles;
-
     private void Start()
     {
         //UpdateColoursIfColourBlindMode();
 
+        // Creates a new conversation handler object and sets whether to save previous messages or not
+        // depending on the user's current settings 
         currentConversation = new ConversationHandler();
         currentConversation.setSaveMessages(currentSettings.ReturnFieldValue("saveConversations"));
 
-        templateUserSpeechBubble = GameObject.FindGameObjectsWithTag("SpeechBubbleUser")[0];
-        templateChatbotSpeechBubble = GameObject.FindGameObjectsWithTag("SpeechBubbleChatbot")[0];
+        // Retrieves the conversation renderer script
+        GameObject conversationObject = GameObject.Find("ConversationRenderer");
+        ConversationRenderer conversationRenderer = conversationObject.GetComponent<ConversationRenderer>();
 
-        templateUserSpeechBubble.SetActive(false);
-        templateChatbotSpeechBubble.SetActive(false);
+        // Adds the default greetings message
+        currentConversation.AddNewMessage("Hello there, how can I help you today?", false);
 
-        distanceBetweenSpeechBubbles = 100;
-        float defaultSpeechBubbleHeight = 200;
-
-        currentConversationHeadPosition = templateChatbotSpeechBubble.transform.position.y;
-
-        Vector3 newSpeechBubblePosition = templateChatbotSpeechBubble.transform.position;
-        newSpeechBubblePosition.y = currentConversationHeadPosition;
-        AddNewSpeechBubble(newSpeechBubblePosition, false, "Hello there, how can I help you today?");
-
-        UpdateConversationHeadPosition(defaultSpeechBubbleHeight);
+        // Configures and renders all messages in the conversation
+        conversationRenderer.ConfigureConversation();
+        conversationRenderer.SetConversation(currentConversation);
+        conversationRenderer.RenderConversation();
     }
 
     private void Update()
     {
 
     }
-
-    private void AddNewSpeechBubble(Vector3 position, bool isUserSpeaker, string message)
-    {
-        RectTransform canvasRectTransform = GameObject.Find("ConversationContainer").GetComponent<RectTransform>();
-
-        GameObject speechBubbleToClone;
-        if (isUserSpeaker)
-            speechBubbleToClone = templateUserSpeechBubble;
-        else
-            speechBubbleToClone = templateChatbotSpeechBubble;
-
-        GameObject newSpeechBubble = Instantiate(speechBubbleToClone, position, templateUserSpeechBubble.transform.rotation);
-
-        newSpeechBubble.SetActive(true);
-
-        newSpeechBubble.transform.SetParent(GameObject.Find("ConversationContainer").transform);
-
-        newSpeechBubble.transform.GetChild(0).GetComponent<UnityEngine.UI.Text>().text = message;
-    }
-
-    private void UpdateConversationHeadPosition(float speechBubbleHeight)
-    {
-        currentConversationHeadPosition -= speechBubbleHeight / 2 + distanceBetweenSpeechBubbles;
-    }
-
 }
--- Assets/Scripts/PreviousConversations.cs
+++ Assets/Scripts/PreviousConversations.cs
@@ -8,116 +8,28 @@ public class PreviousConversations: BaseUIScene
 {
     private ConversationHandler prevConversation;
 
-    private float currentConversationHeadPosition;
-
-    private GameObject templateUserSpeechBubble;
-
-    private GameObject templateChatbotSpeechBubble;
-
-    private float distanceBetweenSpeechBubbles;
-
     private void Start()
     {
         SetFade();
         SetFadeInSpeed(0.66f);
         UpdateColoursIfColourBlindMode();
 
-        ConfigureConversation();
-        RenderConversation();
+        // Creates a new conversation handler object and loads all previous saved messages
+        prevConversation = new ConversationHandler();
+        prevConversation.LoadPrevConversation();
+
+        // Retrieves the conversation renderer script
+        GameObject conversationObject = GameObject.Find("ConversationRenderer");
+        ConversationRenderer conversationRenderer = conversationObject.GetComponent<ConversationRenderer>();
+
+        // Configures and renders all messages in the previous conversation
+        conversationRenderer.ConfigureConversation();
+        conversationRenderer.SetConversation(prevConversation);
+        conversationRenderer.RenderConversation();
     }
 
     private void Update()
     {
         UpdateFade();
-    }
-
-    private void ConfigureConversation()
-    {
-        prevConversation = new ConversationHandler();
-        prevConversation.LoadPrevConversation();
-
-
-
-        templateUserSpeechBubble = GameObject.FindGameObjectsWithTag("SpeechBubbleUser")[0];
-        templateChatbotSpeechBubble = GameObject.FindGameObjectsWithTag("SpeechBubbleChatbot")[0];
-
-        templateUserSpeechBubble.SetActive(false);
-        templateChatbotSpeechBubble.SetActive(false);
-
-        distanceBetweenSpeechBubbles = 100;
-
-    }
-
-
-    private void RenderConversation()
-    {
-        int numMessages = prevConversation.GetCurrentConversationSize();
-
-        if (numMessages < 1)
-            return;
-
-        float defaultSpeechBubbleHeight = 200.0f; 
-
-        Message newMessage = prevConversation.GetNewMessageAtIndex(0);
-        Vector3 newSpeechBubblePosition;
-
-        if (newMessage.userWasSpeaker)
-        {
-            currentConversationHeadPosition = templateUserSpeechBubble.transform.position.y;
-            newSpeechBubblePosition = templateUserSpeechBubble.transform.position;
-        }
-        else
-        {
-            currentConversationHeadPosition = templateChatbotSpeechBubble.transform.position.y;
-            newSpeechBubblePosition = templateChatbotSpeechBubble.transform.position;
-        }
-
-        newSpeechBubblePosition.y = currentConversationHeadPosition;
-        AddNewSpeechBubble(newSpeechBubblePosition, newMessage.userWasSpeaker, newMessage.text);
-
-        UpdateConversationHeadPosition(defaultSpeechBubbleHeight);
-
-        for (int i = 1; i < numMessages; i++)
-            {
-                newMessage = prevConversation.GetNewMessageAtIndex(i);
-
-            if (newMessage.userWasSpeaker)
-                newSpeechBubblePosition = templateUserSpeechBubble.transform.position;
-            else
-                newSpeechBubblePosition = templateChatbotSpeechBubble.transform.position;
-
-            newSpeechBubblePosition.y = currentConversationHeadPosition;
-            AddNewSpeechBubble(newSpeechBubblePosition, newMessage.userWasSpeaker, newMessage.text);
-
-            UpdateConversationHeadPosition(defaultSpeechBubbleHeight);
-        }
-
-
-
-    }
-
-
-    private void AddNewSpeechBubble(Vector3 position, bool isUserSpeaker, string message)
-    {
-        RectTransform canvasRectTransform = GameObject.Find("ConversationContainer").GetComponent<RectTransform>();
-
-        GameObject speechBubbleToClone;
-        if (isUserSpeaker)
-            speechBubbleToClone = templateUserSpeechBubble;
-        else
-            speechBubbleToClone = templateChatbotSpeechBubble;
-
-        GameObject newSpeechBubble = Instantiate(speechBubbleToClone, position, templateUserSpeechBubble.transform.rotation);
-
-        newSpeechBubble.SetActive(true);
-
-        newSpeechBubble.transform.SetParent(GameObject.Find("ConversationContainer").transform);
-
-        newSpeechBubble.transform.GetChild(0).GetComponent<UnityEngine.UI.Text>().text = message;
-    }
-
-    private void UpdateConversationHeadPosition(float speechBubbleHeight)
-    {
-        currentConversationHeadPosition -= speechBubbleHeight / 2 + distanceBetweenSpeechBubbles;
     }
 }
