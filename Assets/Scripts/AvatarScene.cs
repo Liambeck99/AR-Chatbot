c40e288f41ce45f6187b0bc75d8339025b0390f5
Lewis Stuart <lewis.stuart11@yahoo.co.uk>
2022-02-19 03:21:26 +0000

    Added messages showing up in a 3D text box

--- Assets/Scripts/AvatarScene.cs
+++ Assets/Scripts/AvatarScene.cs
@@ -6,6 +6,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 using Random = UnityEngine.Random;
 
@@ -23,10 +24,26 @@ public class AvatarScene : BaseSessionScene
 
     private bool inIntroAnimation;
 
-    private float introTime;
+    private float introAnimationDurationTime;
 
     private Vector3 FinalPosition;
     private Vector3 BeginningPosition;
+
+    private Vector3 SidePosition;
+
+    GameObject chatbotSpeechBubble;
+    GameObject responseContainer;
+    GameObject speechBubbleScaler;
+    TextMeshPro responseText;
+
+    bool executeSpeechBubbleAnimation;
+    bool reverseSpeechBubbleAnimation;
+    float speechBubbleAnimationStartTime;
+    float speechBubbleAnimationDurationTime;
+
+    int charsPerSecond;
+
+    string currentMessage;
 
     private void Start()
     {
@@ -46,9 +63,11 @@ public class AvatarScene : BaseSessionScene
         else
             animationController.MuteAudio();
 
-        introTime = 2.5f;
+        introAnimationDurationTime = 2.5f;
         FinalPosition = new Vector3(-0.05f, -2.0f, -0.5f);
         BeginningPosition = new Vector3(-0.05f, -0.7f, 20.0f);
+
+        SidePosition = new Vector3(1.0f, -2.0f, -1.0f);
 
         // There is a session in progress, therefore do not play intro animation
         if (currentSessionHandler.currentConversation.GetCurrentConversationSize() > 1)
@@ -60,6 +79,20 @@ public class AvatarScene : BaseSessionScene
         else
             inIntroAnimation = true;
 
+        chatbotSpeechBubble = GameObject.Find("ChatbotSpeechBubble");
+        responseText = GameObject.Find("ResponseText").GetComponent<TextMeshPro>();
+
+        responseContainer = GameObject.Find("ResponseContainer");
+        speechBubbleScaler = GameObject.Find("SpeechBubbleScaler");
+
+        responseContainer.SetActive(false);
+
+        executeSpeechBubbleAnimation = false;
+        reverseSpeechBubbleAnimation = false;
+        speechBubbleAnimationDurationTime = 0.5f;
+        speechBubbleAnimationStartTime = Time.time;
+
+        charsPerSecond = 6;
     }
 
     private void Update()
@@ -70,12 +103,11 @@ public class AvatarScene : BaseSessionScene
 
         if (inIntroAnimation)
         {
-            float percentage = (introTime - Time.time)/introTime;
+            float percentage = (introAnimationDurationTime - Time.time)/introAnimationDurationTime;
 
             if(percentage > 0)
-            {
                 xBotModel.transform.position = (FinalPosition * (1 - percentage)) + (BeginningPosition * (percentage));
-            }
+            
             else
             {
                 inIntroAnimation = false;
@@ -83,6 +115,38 @@ public class AvatarScene : BaseSessionScene
             }
         }
 
+        if (executeSpeechBubbleAnimation && !reverseSpeechBubbleAnimation)
+        {
+
+            float percentage = ((speechBubbleAnimationStartTime + speechBubbleAnimationDurationTime) - Time.time) / speechBubbleAnimationDurationTime;
+
+            int lineCount = responseText.textInfo.lineCount;
+
+            if (percentage > 0)
+            {
+                speechBubbleScaler.transform.localScale = new Vector3(1.0f * (1 - percentage),
+                                                                     ((float)lineCount / 6.5f) * (1 - percentage),
+                                                                     1.0f * (1 - percentage));
+
+                xBotModel.transform.position = (SidePosition * (1 - percentage)) + (FinalPosition * (percentage));
+            }
+
+            else
+            {
+                responseText.color = new Color32(255, 255, 255, 255);
+                executeSpeechBubbleAnimation = false;
+            }
+        }
+
+        if (executeSpeechBubbleAnimation && reverseSpeechBubbleAnimation)
+        {
+            float percentage = ((speechBubbleAnimationStartTime + speechBubbleAnimationDurationTime) - Time.time) / speechBubbleAnimationDurationTime;
+
+            if (percentage > 0)
+                xBotModel.transform.position = (SidePosition * percentage) + (FinalPosition * (1 - percentage));
+            else
+                executeSpeechBubbleAnimation = false;
+        }
     }
 
     protected override void RenderUserMessage(string message)
@@ -92,8 +156,18 @@ public class AvatarScene : BaseSessionScene
 
     protected override void RenderChatbotResponseMessage(string message)
     {
-        int charsPerSecond = 10;
         int timeToWait = message.Length / charsPerSecond;
+
+        responseContainer.SetActive(true);
+
+        responseText.text = message;
+
+        executeSpeechBubbleAnimation = true;
+        reverseSpeechBubbleAnimation = false;
+        speechBubbleAnimationStartTime = Time.time;
+
+        responseText.color = new Color32(255, 255, 255, 0);
+
         StartCoroutine(RenderExplinationAnimationWithDelay(timeToWait));
     }
 
@@ -110,6 +184,13 @@ public class AvatarScene : BaseSessionScene
         animationController.ToggleAnimationPhase();
 
         allowInputs = true;
+
+        responseText.text = "";
+
+        executeSpeechBubbleAnimation = true;
+        reverseSpeechBubbleAnimation = true;
+        speechBubbleAnimationStartTime = Time.time;
+        responseContainer.SetActive(false);
     }
 
     protected override void SetColourBlindSprites()
@@ -132,7 +213,7 @@ public class AvatarScene : BaseSessionScene
 
         int chanceOfRandomAnimation = 1;
 
-        if (Random.Range(0, 2000) < chanceOfRandomAnimation)
+        if (Random.Range(0, 1000) < chanceOfRandomAnimation)
         {
             animationController.PerformRandomAnimationIfIdle();
             timeOfLastRandomAnimation = DateTime.Now;
